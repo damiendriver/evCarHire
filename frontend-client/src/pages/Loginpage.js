7e76b02ff0eb57ab5e20a89b66b9dd69aafa329e
driverdamien <driverdamien@hotmail.com>
2024-02-01 01:49:59 +0000

    create login register pages

--- frontend-client/src/pages/Loginpage.js
+++ frontend-client/src/pages/Loginpage.js
@@ -1,10 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Loginpage() {
-    return (
-        <div>
-            <h1>Knock Knock</h1>
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+
+  function Login() {
+    const user = {
+      email,
+      password,
+    };
+    console.log(user);
+  }
+
+  return (
+    <div className="m-5">
+      <div className="row justify-content-center mt-5">
+        <div className="col-md-5 box">
+          <div>
+            <h1>Login</h1>
+
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => {
+                setemail(e.target.value);
+              }}
+            />
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => {
+                setpassword(e.target.value);
+              }}
+            />
+
+            <button className="btn btn-primary mt-3 " onClick={Login}>
+              Login
+            </button>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
-export default Loginpage
+export default Loginpage;
--- frontend-client/src/pages/Registerpage.js
+++ frontend-client/src/pages/Registerpage.js
@@ -1,10 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Registerpage() {
-    return (
-        <div>
-            <h1>Sign Up for Free</h1>
+  const [name, setname] = useState("");
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  const [confirm, setconfirm] = useState("");
+
+  function register() {
+    if (password === confirm) {
+      const user = {
+        name,
+        email,
+        password,
+        confirm,
+      };
+      console.log(user);
+    } else {
+      alert("Passwords do not match. Please double check and try again.");
+    }
+  }
+
+  return (
+    <div className="m-5">
+      <div className="row justify-content-center mt-5">
+        <div className="col-md-5 box">
+          <div>
+            <h1>Register</h1>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Name"
+              value={name}
+              onChange={(e) => {
+                setname(e.target.value);
+              }}
+            />
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => {
+                setemail(e.target.value);
+              }}
+            />
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => {
+                setpassword(e.target.value);
+              }}
+            />
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Confirm Password"
+              value={confirm}
+              onChange={(e) => {
+                setconfirm(e.target.value);
+              }}
+            />
+            <button className="btn btn-primary mt-3 " onClick={register}>
+              Register
+            </button>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
-export default Registerpage
+export default Registerpage;
