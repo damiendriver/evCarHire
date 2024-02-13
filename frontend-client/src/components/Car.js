66b3d66e20added92d39597acc25d884748e1ad6
driverdamien <driverdamien@hotmail.com>
2024-02-13 00:12:30 +0000

    update car display to card grid

--- frontend-client/src/components/Car.js
+++ frontend-client/src/components/Car.js
@@ -6,17 +6,19 @@ function Car({ car, pickupdate, returndate }) {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-  return (
-    <div className="row box" key={car._id}>
-      <div className="col-md-4">
-        <img src={car.imageURLs[0]} className="singleimg" alt="nice car" />
-      </div>
-      <div className="col-md-7">
-        <h1>{car.makeModel}</h1>
-        <p>Car Group:{car.carGroup}</p>
-        <p>Acriss:{car.acriss}</p>
-        <p>Battery:{car.batteryType}</p>
 
+  return (
+    <div className="card box">
+      <img
+        src={car.imageURLs[0]}
+        className="card-img-top"
+        alt={car.makeModel}
+      />
+      <div className="card-body">
+        <h5 className="card-title">{car.makeModel}</h5>
+        <p className="card-text">Car Group: {car.carGroup}</p>
+        <p className="card-text">Acriss: {car.acriss}</p>
+        <p className="card-text">Battery: {car.batteryType}</p>
         <div>
           <Link to={`/book/${car._id}/${pickupdate}/${returndate}`}>
             <button className="btn btn-primary m-3">Book Now</button>
@@ -26,24 +28,17 @@ function Car({ car, pickupdate, returndate }) {
           </button>
         </div>
       </div>
-
       <Modal show={show} onHide={handleClose} size="lg">
         <Modal.Header closeButton>
           <Modal.Title>{car.makeModel}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <Carousel>
-            {car.imageURLs.map((url, index) => {
-              return (
-                <Carousel.Item key={`${url}-${index}`}>
-                  <img
-                    className="d-block w-100 rotaimg"
-                    src={url}
-                    alt={car.makeModel}
-                  />
-                </Carousel.Item>
-              );
-            })}
+            {car.imageURLs.map((url, index) => (
+              <Carousel.Item key={`${url}-${index}`}>
+                <img className="d-block w-100" src={url} alt={car.makeModel} />
+              </Carousel.Item>
+            ))}
           </Carousel>
           <b>
             <p>Vehicle Acriss: {car.acriss}</p>
--- frontend-client/src/pages/Homepage.js
+++ frontend-client/src/pages/Homepage.js
@@ -1,59 +1,41 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Car from "../components/Car";
 import Loading from "../components/Loading";
 import Error from "../components/Error";
-import "antd/dist/reset.css";
 import moment from "moment";
 import { DatePicker } from "antd";
+import Car from "../components/Car";
 
 const { RangePicker } = DatePicker;
 
 function Homepage() {
-  const [cars, setcars] = useState([]);
-  const [loading, setloading] = useState();
-  const [error, seterror] = useState();
-  const [pickupdate, setpickupdate] = useState();
-  const [returndate, setreturndate] = useState();
+  const [cars, setCars] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [pickupdate, setPickupdate] = useState(null);
+  const [returndate, setReturndate] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        setloading(true);
-        const data = (await axios.get("/api/car/getallcars")).data;
-
-        setcars(data);
-        setloading(false);
-
-        console.log(data);
+        setLoading(true);
+        const response = await axios.get("/api/car/getallcars");
+        setCars(response.data);
+        setLoading(false);
       } catch (error) {
-        seterror(true);
-        console.log(error);
-        setloading(false);
+        console.error("Error fetching cars:", error);
+        setError(true);
+        setLoading(false);
       }
     };
 
     fetchData();
-
-    return () => {};
   }, []);
 
   function filterByDate(dates) {
-    console.log("Selected dates:", dates);
-
-    // Ensure dates array contains valid dates
     if (Array.isArray(dates) && dates.length === 2) {
-      console.log(
-        "First selected date:",
-        moment(dates[0].$d).format("DD-MM-YYYY")
-      );
-      console.log(
-        "Second selected date:",
-        moment(dates[1].$d).format("DD-MM-YYYY")
-      );
-
-      setpickupdate(moment(dates[0].$d).format("DD-MM-YYYY"));
-      setreturndate(moment(dates[1].$d).format("DD-MM-YYYY"));
+      setPickupdate(moment(dates[0].$d).format("DD-MM-YYYY"));
+      setReturndate(moment(dates[1].$d).format("DD-MM-YYYY"));
     }
   }
 
@@ -65,20 +47,19 @@ function Homepage() {
         </div>
       </div>
 
-      <div className="row justify-content-center mt-5"></div>
-      {loading ? (
-        <Loading />
-      ) : error ? (
-        <Error />
-      ) : (
-        cars.map((car) => {
-          return (
-            <div key={car._id} className="col-md-9 mt-2">
+      <div className="row justify-content-center mt-5">
+        {loading ? (
+          <Loading />
+        ) : error ? (
+          <Error />
+        ) : (
+          cars.map((car) => (
+            <div key={car._id} className="col-md-4 mb-4">
               <Car car={car} pickupdate={pickupdate} returndate={returndate} />
             </div>
-          );
-        })
-      )}
+          ))
+        )}
+      </div>
     </div>
   );
 }
