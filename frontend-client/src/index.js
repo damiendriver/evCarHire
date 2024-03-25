9544f8fcd1e536d31e14a54883f2f83cb67786ae
Damien Driver <ddriver@hertz.ie>
2024-03-25 13:09:37 +0000

    add vercel analytics

--- frontend-client/src/index.js
+++ frontend-client/src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'leaflet/dist/leaflet.css';
+import { Analytics } from "@vercel/analytics/react"
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Analytics/>
     <App />
   </React.StrictMode>
 );
